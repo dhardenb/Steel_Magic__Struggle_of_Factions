082a906577d24b8aa6e1cafd7223f1809321608d
Bobby Hardenbrook <bobby.hardenbrook@gmail.com>
2014-07-27 01:43:43 -0700

    Faction delete cleanup
    Faction assets are untied from a faction when it is deleted but the
    assets themselves remain

--- faction/final_faction_delete.php
+++ faction/final_faction_delete.php
@@ -8,7 +8,13 @@ $tbl_name="members"; // Table name
 session_register("memberid");
 $memberid = $_SESSION['memberid'];
 
+session_register("factionid");
+$factionid = $_SESSION['factionid'];
+
 $faction_delete_query="delete from factions where factions.memberid = '$memberid'";
+mysql_query($faction_delete_query);
+
+$faction_delete_query="delete from asset_mapping where asset_mapping.factionid = '$factionid'";
 mysql_query($faction_delete_query);
 
 header("location:../main.php");
